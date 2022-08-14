df489c59b3683542e0a3ab5196068f29ed5bb186
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-08-14 12:30:53 -0400

    Add extension method

---
+++ Interject/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Interject.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Interject.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInterject(this ServiceCollection services, ServiceLifetime lifetime, params Assembly[] assemblies)
+    {
+        services.TryAdd(ServiceDescriptor.Describe(typeof(IInterjector), typeof(Interjector), lifetime));
+
+        var handlers = assemblies
+            .SelectMany(a => a.DefinedTypes)
+            .Where(t => t.IsClass && !t.IsAbstract)
+            .Where(dt => dt.ImplementedInterfaces.Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)));
+        
+        foreach (var handler in handlers)
+            services.TryAdd(ServiceDescriptor.Describe(typeof(IRequestHandler<,>), handler, lifetime));
+
+        return services;
+    }
+    
+}
