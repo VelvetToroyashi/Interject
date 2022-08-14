03965acfe31a7754405e169186f5280eca8d93d7
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-08-14 11:18:22 -0400

    Add definition for IInjector

--- Interject/IInterjector.cs
+++ Interject/IInterjector.cs
@@ -1,6 +1,8 @@
-﻿namespace Interject;
+﻿using Interject.Contracts;
+
+namespace Interject;
 
 public interface IInterjector
 {
-    
+    Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken ct = default);
 }
