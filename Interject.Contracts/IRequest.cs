71ed2fc3fd856fb42337b081c47fb8ed435a3139
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-08-15 21:01:58 -0400

    Implement `Unit`

--- Interject.Contracts/IRequest.cs
+++ Interject.Contracts/IRequest.cs
@@ -1,5 +1,12 @@
 ﻿namespace Interject.Contracts;
 
-public interface IRequest { }
+public interface IRequest : IRequest<Unit> { }
 
 public interface IRequest<out T> { }
+
+public readonly record struct Unit
+{
+    private static readonly Unit _ref = new();
+    
+    public static Unit Value => _ref;
+}
